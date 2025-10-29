50bed5c695cf9ca53ab4f3fda23e5f218c4615ee
태현 <78199616+NekoNyangYee@users.noreply.github.com>
2025-10-29 13:13:40 +0900

    mod: webidl-conversions 관련 오류 수정

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
+    environmentOptions: {
+      jsdom: {
+        resources: "usable",
+      },
+    },
+    pool: "forks",
   },
   resolve: {
     alias: {
